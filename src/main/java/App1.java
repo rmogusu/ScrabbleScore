d44fe8688d017ba6c7637a811b2e975bbc78d994
rmogusu <rmogusu123@gmail>
2020-04-16 21:26:41 +0200

    Add

--- src/main/java/App1.java
+++ src/main/java/App1.java
@@ -5,18 +5,18 @@ import models.ScrabbleScore;
 
 
 public class App1 {
-    public static void main(String[] args) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("I'm a Scrabble Score  application!");
-        System.out.println("Enter a word:");
-        try {
-            String stringUserWord = bufferedReader.readLine();
-            ScrabbleScore scrabble = new ScrabbleScore(stringUserWord);
-            int  wordPoint = scrabble.getScore();
-            System.out.println(" The word " + stringUserWord + " : " + " is equal to " + wordPoint + " points");
-
-        }
-        catch (IOException e){
+//    public static void main(String[] args) {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("I'm a Scrabble Score  application!");
+//        System.out.println("Enter a word:");
+//        try {
+//            String stringUserWord = bufferedReader.readLine();
+//            ScrabbleScore scrabble = new ScrabbleScore(stringUserWord);
+//            int  wordPoint = scrabble.getScore();
+//            System.out.println(" The word " + stringUserWord + " : " + " is equal to " + wordPoint + " points");
+//
+//        }
+//        catch (IOException e){
             e.printStackTrace();
         }
     }
